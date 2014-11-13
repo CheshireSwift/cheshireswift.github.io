e81ce7545295bee88741348402c999606f04febe
CheshireSwift <swift@swifteagle.co.uk>
2014-11-13 23:01:31 +0000

    Initial.

---
+++ scripts/scratch.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $( "#core-cell" ).html("Changed!")
+
+})
+
+var addRowTop = function() {
+  $("#core-cell").html("up!")
+}
+
+var addRowBottom = function() {
+  $("#core-cell").html("down!")
+}
