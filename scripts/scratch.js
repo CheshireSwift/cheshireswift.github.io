24dd120a2fc25404be373cdac879a118b4fdbff6
Sam Collard <sam.collard@softwire.com>
2014-12-09 17:20:06 +0000

    Added left/right table expansion.

--- scripts/scratch.js
+++ scripts/scratch.js
@@ -4,9 +4,23 @@ $(document).ready(function(){
 })
 
 var addRowTop = function() {
-  $("#core-cell").html("up!")
+  $("#core-cell").html("up!");
+  var table = document.getElementById("expanding-table")
+  var row = table.insertRow(0);
+  var cell = row.insertCell(0);
+  cell.innerHtml = "<p>new!</p>";
 }
 
 var addRowBottom = function() {
   $("#core-cell").html("down!")
+}
+
+
+var addColumnLeft = function() {
+  $("#core-cell").html("left!")
+}
+
+
+var addColumnRight = function() {
+  $("#core-cell").html("right!")
 }
