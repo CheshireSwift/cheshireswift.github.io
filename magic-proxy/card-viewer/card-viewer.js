01395ba3590e05d29c9ca04f28142fe7dcb327c2
CheshireSwift <swift@swifteagle.co.uk>
2016-05-29 23:32:04 +0100

    Mass refactoring and error resilience

--- magic-proxy/card-viewer/card-viewer.js
+++ magic-proxy/card-viewer/card-viewer.js
@@ -2,7 +2,7 @@
 
 const gameDir = 'images/'
 function setImage(id, pathSuffix) {
-  $(id).attr('src', pathSuffix.includes('undefined') ? '' : gameDir + pathSuffix)
+  $('#' + id).attr('src', pathSuffix.includes('undefined') ? '' : gameDir + pathSuffix)
 }
 
 var parseColor = rawColor => {
@@ -11,6 +11,10 @@ var parseColor = rawColor => {
 }
 
 function pictureCategory(card) {
+  if (!card.type) {
+    return ''
+  }
+
   switch (true) {
     case card.type.includes('Artifact Creature'):
       return 'transport'
@@ -68,6 +72,10 @@ function classForIcon(icon) {
 }
 
 function generateColors(manaCost) {
+  if (!manaCost) {
+    return []
+  }
+
   var cost = manaCost.toLowerCase()
   var retval = []
   if (cost.includes('w')) {
@@ -85,7 +93,60 @@ function generateColors(manaCost) {
   if (cost.includes('g')) {
     retval.push('green')
   }
-  return retval
+
+  return retval.length ? retval : ['c']
+}
+
+function copyField(card, field, opts={}) {
+  if (!card[field]) {
+    $('#' + field).html('')
+    return
+  }
+
+  var text = card[field].replace(/~/g, card.name)
+  if (opts.paras) {
+    text = '<p>' + text.replace(/\n/g, '</p><p>') + '</p>'
+  }
+
+  if (opts.icons) {
+    text = text.replace(/{(.+?)}/gi, iconForAbbreviation)
+  }
+
+  if (opts.longDash) {
+    text = text.replace(/-/g, '\u2014')
+  }
+
+  $('#' + field).html(text)
+}
+
+function getColors(colors, manaCost) {
+  if (colors) {
+    return colors
+  }
+
+  colors = generateColors(manaCost)
+  if (!colors.length) {
+    return ['c']
+  }
+
+  return colors
+}
+
+function getPt(power, toughness, loyalty) {
+  if (power && toughness) {
+    return power + '/' + toughness
+  } else {
+    return loyalty || ''
+  }
+}
+
+function getRarityColor(rarity) {
+  return {
+    m: 'darkorange',
+    r: 'gold',
+    u: 'silver',
+    c: 'black'
+  }[rarity && rarity.substring(0, 1).toLowerCase()]
 }
 
 var cardViewer = {
@@ -97,65 +158,27 @@ var cardViewer = {
       return ex
     }
 
-    copyField('name')
-    copyField('type', {longDash: true})
-    copyField('text', {paras: true, icons: true})
-    copyField('flavor', {paras: true, longDash: true})
-    copyField('manaCost', {icons: true})
-    
-    $('#ptbox').html(
-      card.power && card.toughness ?
-        card.power + '/' + card.toughness :
-        card.loyalty ||
-        ''
-    )
-
-    if (!card.colors) {
-      card.colors = generateColors(card.manaCost)
+    if (typeof card !== 'object') {
+      return 'Card object required'
     }
 
-    if (!card.colors.length) {
-      card.colors.push('c')
-    }
+    copyField(card, 'name')
+    copyField(card, 'type', {longDash: true})
+    copyField(card, 'text', {paras: true, icons: true})
+    copyField(card, 'flavor', {paras: true, longDash: true})
+    copyField(card, 'manaCost', {icons: true})
 
-    setImage('#back-main-img',  `back/${parseColor(card.colors[0])}.jpg`)
-    setImage('#back-half-img',  `back/h${parseColor(card.colors[1])}.png`)
-    setImage('#rules-main-img', `rules/${parseColor(card.colors[0])}.png`)
-    setImage('#rules-half-img', `rules/h${parseColor(card.colors[1])}.png`)
-    setImage('#type-img',       `type/${parseColor(card.colors[0] && !card.colors[1] ? card.colors[0] : 'c')}.png`)
+    $('#ptbox').html(getPt(card.power, card.toughness, card.loaylty))
+
+    var colors = getColors(card.colors, card.manaCost)
+    setImage('back-main-img',  `back/${parseColor(colors[0])}.jpg`)
+    setImage('back-half-img',  `back/h${parseColor(colors[1])}.png`)
+    setImage('rules-main-img', `rules/${parseColor(colors[0])}.png`)
+    setImage('rules-half-img', `rules/h${parseColor(colors[1])}.png`)
+    setImage('type-img',       `type/${parseColor(colors[0] && !colors[1] ? colors[0] : 'c')}.png`)
 
     $('#art-img').attr('src', card.imgUrl || 'http://lorempixel.com/315/140/' + pictureCategory(card))
-
-    $('#rarity').css('color', {
-      m: 'darkorange',
-      r: 'gold',
-      u: 'silver',
-      c: 'black'
-    }[card.rarity.substring(0, 1).toLowerCase()])
-
-    return 'Success!'
-
-    function copyField(field, opts={}) {
-      if (!card[field]) {
-        $('#' + field).html('')
-        return
-      }
-
-      var text = card[field].replace(/~/g, card.name)
-      if (opts.paras) {
-        text = '<p>' + text.replace(/\n/g, '</p><p>') + '</p>'
-      }
-
-      if (opts.icons) {
-        text = text.replace(/{(.+?)}/gi, iconForAbbreviation)
-      }
-
-      if (opts.longDash) {
-        text = text.replace(/-/g, '\u2014')
-      }
-
-      $('#' + field).html(text)
-    }
+    $('#rarity').css('color', getRarityColor(card.rarity))
   }
 }
 
